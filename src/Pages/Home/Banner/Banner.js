500fea5fd95774d065a9ed0d1f32e178255fb024
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2022-11-25 20:21:10 +0600

    banner-sectin-complete

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,27 +1,46 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
-import carImg from '../../../images/car.jpg'
+import { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import banner from '../../../images/banner.jpg';
+import banner2 from '../../../images/banner2.jpg';
+import banner3 from '../../../images/banner3.jpg';
+import './Banner.css';
 const Banner = () => {
     return (
-        <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full">
-        <div className="hero">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src={carImg} className="w-1/4 rounded-lg shadow-2xl" />
-    <div className='w-1/3'>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
-  </div>
-</div>
-          
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" className="btn btn-circle">❮</a> 
-            <a href="#slide2" className="btn btn-circle">❯</a>
-          </div>
-        </div> 
-     
-      </div>
+   <div>
+       <Carousel>
+                <div className='carouselParent'>
+                    <img src={banner} />
+                   <div className='carouselChild'>
+                   <h2 className=' top-0 left-0 transform- text-white font-bold text-5xl mb-3 text-[#FF4848] uppercase'>Your Dream Car here </h2>
+                   <p className='text-white text-xl text-[#FAB637]'>
+                   Automobiles are marketed under the brands BMW, Mini and Rolls-Royce, and motorcycles are marketed under the brand BMW Motorrad. In 2017, BMW was the world's fourteenth-largest producer of motor vehicles, with 2,279,503 vehicles produced.[2] The company has significant motor-sport history, especially in touring cars, sports cars, and the Isle of Man TT.
+                   </p>
+                   </div>
+                </div>
+                <div className='carouselParent'>
+                    <img src={banner2} />
+                   <div className='carouselChild'>
+                   <h2 className=' top-0 left-0 transform- text-white font-bold text-5xl mb-3 text-[#FF4848] uppercase'>Your Dream Car here </h2>
+                   <p className='text-white text-xl text-[#FAB637]'>
+                   Automobiles are marketed under the brands BMW, Mini and Rolls-Royce, and motorcycles are marketed under the brand BMW Motorrad. In 2017, BMW was the world's fourteenth-largest producer of motor vehicles, with 2,279,503 vehicles produced.[2] The company has significant motor-sport history, especially in touring cars, sports cars, and the Isle of Man TT.
+                   </p>
+                   </div>
+                </div>
+                <div className='carouselParent'>
+                    <img src={banner3} />
+                   <div className='carouselChild'>
+                   <h2 className=' top-0 left-0 transform- text-white font-bold text-5xl mb-3 text-[#FF4848] uppercase'>Your Dream Car here </h2>
+                   <p className='text-white text-xl text-[#FAB637]'>
+                   Automobiles are marketed under the brands BMW, Mini and Rolls-Royce, and motorcycles are marketed under the brand BMW Motorrad. In 2017, BMW was the world's fourteenth-largest producer of motor vehicles, with 2,279,503 vehicles produced.[2] The company has significant motor-sport history, especially in touring cars, sports cars, and the Isle of Man TT.
+                   </p>
+                   </div>
+                </div>
+            </Carousel>
+   </div>
     );
 };
 
